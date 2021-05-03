fc0bab96126e6d77197976322340bb7b4c318fd9
GillesRitchmond <gritchmond@gmail.com>
2021-05-03 16:32:42 -0400

    All in index.html

--- View/index.php
+++ View/index.php
@@ -1,7 +1,7 @@
 <!Doctype html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="Style/style.css">
+        <link rel="stylesheet" type="text/css" href="View/Style/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
